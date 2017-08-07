f44a020539fcc95ecafb44df9e9b2e873ea7d641
Edwin Mangiante <edwin_mangiante@hotmail.com>
2017-08-07 15:34:20 -0300

    Muestra grilla de Paises, muestra la ventana modal para dar de alta y editar. Falta probar las 3 funcionalidades (abm) y de ahí, ver si se puede filtrar la grilla  ordenar.

--- ABMCEmpleados/Controllers/PaisController.cs
+++ ABMCEmpleados/Controllers/PaisController.cs
@@ -23,9 +23,71 @@ namespace ABMCEmpleados.Controllers
         {
             using (EmpDBEntities obj = new EmpDBEntities())
             {
-                List<Pais> paises = obj.Paises.ToList();
+                obj.Configuration.LazyLoadingEnabled = false;
+                List<Pais> paises = obj.Paises.OrderBy(x => x.pai_codigo).ToList();
                 return Json(paises, JsonRequestBehavior.AllowGet);
             }
+        }
+
+        /// <summary>  
+        /// Insert New Pais  
+        /// </summary>  
+        /// <param name="Pais"></param>  
+        /// <returns></returns>  
+        public string Insert(Pais Pais)
+        {
+            if (Pais != null)
+                using (EmpDBEntities Obj = new EmpDBEntities())
+                {
+                    Obj.Paises.Add(Pais);
+                    Obj.SaveChanges();
+                    return "Se agregó el país satisfactoriamente.";
+                }
+            else
+                return "No se pudo agregar el país, intente nuevamente.";
+        }
+
+        /// <summary>  
+        /// Delete Pais
+        /// </summary>  
+        /// <param name="Pais"></param>  
+        /// <returns></returns>  
+        public string Delete(Pais Pais)
+        {
+            if (Pais != null)
+                using (EmpDBEntities Obj = new EmpDBEntities())
+                {
+                    var Pais_ = Obj.Entry(Pais);
+                    if (Pais_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Paises.Attach(Pais);
+                        Obj.Paises.Remove(Pais);
+                    }
+                    Obj.SaveChanges();
+                    return "Se eliminó el país satisfactoriamente.";
+                }
+            else
+                return "No se pudo eliminar el país, intente nuevamente.";
+        }
+
+        /// <summary>  
+        /// Update Pais  
+        /// </summary>  
+        /// <param name="Pais"></param>  
+        /// <returns></returns>  
+        public string Update(Pais Pais)
+        {
+            if (Pais != null)
+                using (EmpDBEntities Obj = new EmpDBEntities())
+                {
+                    var Pais_ = Obj.Entry(Pais);
+                    Pais PaisObj = Obj.Paises.Where(x => x.pai_codigo == Pais.pai_codigo).FirstOrDefault();
+                    PaisObj.pai_nombre = Pais.pai_nombre;
+                    Obj.SaveChanges();
+                    return "Se actualizó el país satisfactoriamente.";
+                }
+            else
+                return "No se pudo actualizar el país, intente nuevamente.";
         }
     }
 }
---
+++ ABMCEmpleados/Scripts/CRUDPais.js
@@ -0,0 +1,88 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    //debugger;
+    $scope.Add = function () {
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Agregar") {
+            $scope.Pais = {};
+            $scope.Pais.pai_codigo = $scope.CodigoPais;
+            $scope.Pais.pai_nombre = $scope.NombrePais;
+            $http({
+                method: "post",
+                url: "/Pais/Insert",
+                datatype: "json",
+                data: JSON.stringify($scope.Pais)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAll();
+                $scope.CodigoPais = "";
+                $scope.NombrePais = "";
+            })
+        } else {
+            $scope.Pais = {};
+            $scope.Pais.pai_codigo = $scope.CodigoPais;
+            $scope.Pais.pai_nombre = $scope.NombrePais;
+            $http({
+                method: "post",
+                url: "/Pais/Update",
+                datatype: "json",
+                data: JSON.stringify($scope.Pais)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAll();
+                $scope.CodigoPais = "";
+                $scope.NombrePais = "";
+                document.getElementById("btnSave").setAttribute("value", "Agregar");
+            })
+        }
+    };
+
+    $scope.GetAll = function () {
+        //debugger;
+        $http({
+            method: "get",
+            url: "/Pais/GetAll"
+        }).then(function (response) {
+            //console.log(response);
+            $scope.paises = response.data;
+        }).catch(function (reason) {
+            console.log(reason);
+            alert("Ocurrió un error al intentar obtener los países");
+        })
+    };
+
+    $scope.Delete = function (Pais) {
+        $http({
+            method: "post",
+            url: "/Pais/Delete",
+            datatype: "json",
+            data: JSON.stringify(Pais)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.GetAll();
+        })
+    };
+
+    $scope.Get = function (Pais) {
+        //console.log(Pais);
+        $scope.CodigoPais = Pais.pai_codigo;
+        $('#inputCodigo').prop('readonly', true);
+        $scope.NombrePais = Pais.pai_nombre;
+        document.getElementById("btnSave").setAttribute("value", "Actualizar");
+        $('#myModal').modal('show');
+    }
+})
+
+//esconde el modal cuando se hace click en el botón cerrar o en la cruz.
+function dismissModal() {
+    $('#myModal').modal('hide');
+}
+
+//limpia los textbox.
+function clearTextBox() {
+    //console.log("llegue!!");
+    $('#inputCodigo').val("");
+    $('#inputCodigo').prop('readonly', false);
+    $('#inputNombre').val("");
+    document.getElementById("btnSave").setAttribute("value", "Agregar");
+}
