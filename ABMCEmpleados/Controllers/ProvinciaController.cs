41862e931856b71a1f602740e50b852d5e0a87d6
Edwin Mangiante <edwin_mangiante@hotmail.com>
2017-08-11 15:35:54 -0300

    ABMC de Provincias, validando que no se dejen campos vacíos, ni que se intente ingresar un código existente. Funciona todo. Vengo arrantrando un pequeño bug, presiono en el botón de editar un país o provincia (lo muestra correctamente), cierro y presiono el botón + de agregar, abre la ventana modal sin problemas, la cierro y vuelvo a presionar el botón editar del mismo país o provincia (sólo ocurre con el mismo país o provincia) y me muestra todo bien, pero los textbox vacíos.

--- ABMCEmpleados/Controllers/ProvinciaController.cs
+++ ABMCEmpleados/Controllers/ProvinciaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ABMCEmpleados.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,136 @@ namespace ABMCEmpleados.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Obtiene todas las Provincias cargadas en la BD
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetAll()
+        {
+            using (EmpDBEntities obj = new EmpDBEntities())
+            {
+                obj.Configuration.LazyLoadingEnabled = false;
+                List<Provincia> provincias = obj.Provincias.OrderBy(x => x.pro_pai_codigo).ThenBy(x => x.pro_codigo).ToList();
+                return Json(provincias, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// Obtiene todos los Países cargados en la BD
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetPaises()
+        {
+            using (EmpDBEntities obj = new EmpDBEntities())
+            {
+                obj.Configuration.LazyLoadingEnabled = false;
+                List<Pais> paises = obj.Paises.OrderBy(x => x.pai_codigo).ToList();
+                return Json(paises, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// Obtiene todas las Provincias por País
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetAllByPais(string codigoPais)
+        {
+            if (!string.IsNullOrEmpty(codigoPais))
+                using (EmpDBEntities obj = new EmpDBEntities())
+                {
+                    obj.Configuration.LazyLoadingEnabled = false;
+                    List<Provincia> provincias = obj.Provincias.Where(x => x.pro_pai_codigo == codigoPais).OrderBy(x => x.pro_pai_codigo).ThenBy(x => x.pro_codigo).ToList();
+                    return Json(provincias, JsonRequestBehavior.AllowGet);
+                }
+            else
+                return Json(null, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Obtiene una provincia por clave primaria.
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetByKey(string codigoPais, string codigoProvincia)
+        {
+            //return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+            if (!string.IsNullOrWhiteSpace(codigoPais) && !string.IsNullOrWhiteSpace(codigoProvincia))
+            {
+                using (EmpDBEntities obj = new EmpDBEntities())
+                {
+                    obj.Configuration.LazyLoadingEnabled = false;
+                    IQueryable<Provincia> provincias = obj.Provincias.AsQueryable();
+                    Provincia provincia = provincias.Where(x => x.pro_pai_codigo == codigoPais && x.pro_codigo == codigoProvincia).FirstOrDefault();
+                    if (provincia != null && !string.IsNullOrWhiteSpace(provincia.pro_pai_codigo) && !string.IsNullOrWhiteSpace(provincia.pro_codigo))
+                        return Json(new { existe = true }, JsonRequestBehavior.AllowGet);
+                    else
+                        return Json(new { existe = false }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+                return Json(new { existe = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>  
+        /// Insert New Provincia  
+        /// </summary>  
+        /// <param name="Provincia"></param>  
+        /// <returns></returns>  
+        public string Insert(Provincia Provincia)
+        {
+            if (Provincia != null)
+                using (EmpDBEntities Obj = new EmpDBEntities())
+                {
+                    Obj.Provincias.Add(Provincia);
+                    Obj.SaveChanges();
+                    return "Se agregó la provincia satisfactoriamente.";
+                }
+            else
+                return "No se pudo agregar la provincia, intente nuevamente.";
+        }
+
+        /// <summary>  
+        /// Delete Provincia
+        /// </summary>  
+        /// <param name="Provincia"></param>  
+        /// <returns></returns>  
+        public string Delete(Provincia Provincia)
+        {
+            if (Provincia != null)
+                using (EmpDBEntities Obj = new EmpDBEntities())
+                {
+                    var Provincia_ = Obj.Entry(Provincia);
+                    if (Provincia_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Provincias.Attach(Provincia);
+                        Obj.Provincias.Remove(Provincia);
+                    }
+                    Obj.SaveChanges();
+                    return "Se eliminó la provincia satisfactoriamente.";
+                }
+            else
+                return "No se pudo eliminar la provincia, intente nuevamente.";
+        }
+
+        /// <summary>  
+        /// Update Provincia  
+        /// </summary>  
+        /// <param name="Provincia"></param>  
+        /// <returns></returns>  
+        public string Update(Provincia Provincia)
+        {
+            if (Provincia != null)
+                using (EmpDBEntities Obj = new EmpDBEntities())
+                {
+                    var Provincia_ = Obj.Entry(Provincia);
+                    Provincia ProvinciaObj = Obj.Provincias.Where(x => x.pro_pai_codigo == Provincia.pro_pai_codigo && x.pro_codigo == Provincia.pro_codigo).FirstOrDefault();
+                    ProvinciaObj.pro_nombre = Provincia.pro_nombre;
+                    Obj.SaveChanges();
+                    return "Se actualizó la provincia satisfactoriamente.";
+                }
+            else
+                return "No se pudo actualizar la provincia, intente nuevamente.";
         }
     }
 }
---
+++ ABMCEmpleados/Scripts/CRUDProvincia.js
@@ -0,0 +1,198 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    //debugger;
+    $http({
+        method: 'GET',
+        url: '/Provincia/GetPaises'
+    }).then(function (response) {
+        //console.log(response);
+        $scope.paises = response.data;
+        $scope.currentPais = $scope.paises[0].pai_codigo;
+        $scope.GetAllByPais($scope.paises[0].pai_codigo);
+    }).catch(function (reason) {
+        console.log(reason);
+        alert("Ocurrió un error al intentar obtener los países.");
+    })
+
+    //debugger;
+    $scope.Add = function () {
+        var isValid = Validate();
+
+        if (isValid == true) {
+            var Action = $("#btnSave").val();
+            if (Action == "Agregar") {
+                var pro_pai_codigo = $('#inputPais').val().trim();
+                var pro_codigo = $('#inputCodigo').val().trim();
+                $.ajax({
+                    type: "GET",
+                    url: "/Provincia/GetByKey",
+                    contentType: "application/json;charset=UTF-8",
+                    dataType: "json",
+                    data: ({ codigoPais: pro_pai_codigo, codigoProvincia: pro_codigo }),
+                    success: function (data) {
+                        debugger;
+                        if (data.existe == false) {
+                            $scope.Provincia = {};
+                            console.log($('#inputPais').val());
+                            $scope.Provincia.pro_pai_codigo = $scope.currentPais;
+                            $scope.Provincia.pro_codigo = $scope.CodigoProvincia;
+                            $scope.Provincia.pro_nombre = $scope.NombreProvincia;
+                            $http({
+                                method: "POST",
+                                url: "/Provincia/Insert",
+                                datatype: "json",
+                                data: JSON.stringify($scope.Provincia)
+                            }).then(function (response) {
+                                alert(response.data);
+                                $scope.GetAllByPais($scope.currentPais);
+                                $scope.Provincia.pro_pai_codigo = "";
+                                $scope.Provincia.pro_codigo = "";
+                                $scope.Provincia.pro_nombre = "";
+                                $('#myModal').modal('hide');
+                            })
+                        } else {
+                            alert("El código de provincia ingresado ya existe.");
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        console.log(xhr);
+                        console.log(status);
+                        console.log(error);
+                        a = xhr.responseText;
+                        alert("Ocurrió un error al intentar verificar el código de provincia.");
+                    }
+                })
+            } else {
+                var pro_pai_codigo = $('#inputPais').val().trim();
+                var pro_codigo = $('#inputCodigo').val().trim();
+                $.ajax({
+                    type: "GET",
+                    url: "/Provincia/GetByKey",
+                    contentType: "application/json;charset=UTF-8",
+                    dataType: "json",
+                    data: ({ codigoPais: pro_pai_codigo, codigoProvincia: pro_codigo }),
+                    success: function (data) {
+                        if (data.existe == true) {
+                            $scope.Provincia = {};
+                            $scope.Provincia.pro_pai_codigo = $scope.currentPais;
+                            $scope.Provincia.pro_codigo = $scope.CodigoProvincia;
+                            $scope.Provincia.pro_nombre = $scope.NombreProvincia;
+                            $http({
+                                method: "POST",
+                                url: "/Provincia/Update",
+                                datatype: "json",
+                                data: JSON.stringify($scope.Provincia)
+                            }).then(function (response) {
+                                alert(response.data);
+                                $scope.GetAllByPais($scope.currentPais);
+                                $scope.Provincia.pro_pai_codigo = "";
+                                $scope.Provincia.pro_codigo = "";
+                                $scope.Provincia.pro_nombre = "";
+                                $("#btnSave").attr("value", "Agregar");
+                                $('#myModal').modal('hide');
+                            })
+                        } else {
+                            alert("El código de provincia ingresado no existe.");
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        console.log(xhr);
+                        console.log(status);
+                        console.log(error);
+                        a = xhr.responseText;
+                        alert("Ocurrió un error al intentar verificar el código de provincia.");
+                    }
+                })
+            }
+        }
+    };
+
+    $scope.GetAll = function () {
+        //debugger;
+        $http({
+            method: "GET",
+            url: "/Provincia/GetAll"
+        }).then(function (response) {
+            //console.log(response);
+            $scope.provincias = response.data;
+        }).catch(function (reason) {
+            console.log(reason);
+            alert("Ocurrió un error al intentar obtener las provincias.");
+        })
+    };
+
+    $scope.GetAllByPais = function (pai_codigo) {
+        //debugger;
+        $http({
+            method: "GET",
+            url: "/Provincia/GetAllByPais",
+            params: ({ codigoPais: pai_codigo })
+        }).then(function (response) {
+            //console.log(response);
+            $scope.provincias = response.data;
+        }).catch(function (reason) {
+            console.log(reason);
+            alert("Ocurrió un error al intentar obtener las provincias por país.");
+        })
+    };
+
+    $scope.Delete = function (Provincia) {
+        var ans = confirm('¿Está seguro de qué quiere eliminar la provincia?');
+        if (ans) {
+            $http({
+                method: "POST",
+                url: "/Provincia/Delete",
+                datatype: "json",
+                data: JSON.stringify(Provincia)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllByPais($scope.currentPais);
+            })
+        }
+    };
+
+    $scope.Get = function (Provincia) {
+        $scope.CodigoPais = Provincia.pro_pai_codigo;
+        $('#inputPais').prop('readonly', true);
+        $scope.CodigoProvincia = Provincia.pro_codigo;
+        $('#inputCodigo').prop('readonly', true);
+        $scope.NombreProvincia = Provincia.pro_nombre;
+        $("#btnSave").attr("value", "Actualizar");
+        $('#myModal').modal('show');
+    }
+})
+
+//función para validar que los campos esten completos
+function Validate() {
+    var isValid = true;
+    if ($('#inputPais').val().trim() == "") {
+        alert("Debe completar el código del país de la provincia.");
+        isValid = false;
+    }
+    if ($('#inputCodigo').val().trim() == "") {
+        alert("Debe completar el código de provincia.");
+        isValid = false;
+    }
+    if ($('#inputNombre').val().trim() == "") {
+        alert("Debe completar el nombre del provincia.");
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+//esconde el modal cuando se hace click en el botón cerrar o en la cruz.
+function dismissModal() {
+    $('#myModal').modal('hide');
+}
+
+//limpia los textbox.
+function clearTextBox(codigoPais) {
+    $('#inputPais').val("");
+    $('#inputPais').val(codigoPais.trim());
+    $('#inputPais').prop('readonly', true);
+    $('#inputCodigo').val("");
+    $('#inputCodigo').prop('readonly', false);
+    $('#inputNombre').val("");
+    $("#btnSave").attr("value", "Agregar");
+}
