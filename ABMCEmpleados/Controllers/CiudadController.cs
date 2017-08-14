9c9e1db3a5b410b7a54d875b1da4e2cbc2c15a45
Edwin Mangiante <edwin_mangiante@hotmail.com>
2017-08-14 11:23:21 -0300

    Se ABMC de Ciudad. Funciona bien.

--- ABMCEmpleados/Controllers/CiudadController.cs
+++ ABMCEmpleados/Controllers/CiudadController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ABMCEmpleados.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,152 @@ namespace ABMCEmpleados.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Obtiene todas las ciudades cargadas en la BD
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetAll()
+        {
+            using (EmpDBEntities obj = new EmpDBEntities())
+            {
+                obj.Configuration.LazyLoadingEnabled = false;
+                List<Ciudad> ciudades = obj.Ciudades.OrderBy(x => x.ciu_pro_pai_codigo).ThenBy(x => x.ciu_pro_codigo).ThenBy(x => x.ciu_codigo).ToList();
+                return Json(ciudades, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// Obtiene todos los países cargados en la BD
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetPaises()
+        {
+            using (EmpDBEntities obj = new EmpDBEntities())
+            {
+                obj.Configuration.LazyLoadingEnabled = false;
+                List<Pais> paises = obj.Paises.OrderBy(x => x.pai_codigo).ToList();
+                return Json(paises, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// Obtiene todos las provincias por paises cargadas en la BD
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetProvincias(string codigoPais)
+        {
+            if (!string.IsNullOrEmpty(codigoPais))
+                using (EmpDBEntities obj = new EmpDBEntities())
+                {
+                    obj.Configuration.LazyLoadingEnabled = false;
+                    List<Provincia> provincias = obj.Provincias.Where(x => x.pro_pai_codigo == codigoPais).OrderBy(x => x.pro_pai_codigo).ThenBy(x => x.pro_codigo).ToList();
+                    return Json(provincias, JsonRequestBehavior.AllowGet);
+                }
+            else
+                return Json(null, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Obtiene todas las Provincias por País
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetAllByPaisAndProvincia(string codigoPais, string codigoProvincia)
+        {
+            if (!string.IsNullOrEmpty(codigoPais) && !string.IsNullOrEmpty(codigoProvincia))
+                using (EmpDBEntities obj = new EmpDBEntities())
+                {
+                    obj.Configuration.LazyLoadingEnabled = false;
+                    List<Ciudad> ciudades = obj.Ciudades.Where(x => x.ciu_pro_pai_codigo == codigoPais && x.ciu_pro_codigo == codigoProvincia).OrderBy(x => x.ciu_pro_pai_codigo).ThenBy(x => x.ciu_pro_codigo).ThenBy(x => x.ciu_codigo).ToList();
+                    return Json(ciudades, JsonRequestBehavior.AllowGet);
+                }
+            else
+                return Json(null, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Obtiene una ciudad por clave primaria.
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetByKey(string codigoPais, string codigoProvincia, string codigoCiudad)
+        {
+            if (!string.IsNullOrWhiteSpace(codigoPais) && !string.IsNullOrWhiteSpace(codigoProvincia) && !string.IsNullOrWhiteSpace(codigoCiudad))
+            {
+                using (EmpDBEntities obj = new EmpDBEntities())
+                {
+                    obj.Configuration.LazyLoadingEnabled = false;
+                    IQueryable<Ciudad> ciudades = obj.Ciudades.AsQueryable();
+                    Ciudad ciudad = ciudades.Where(x => x.ciu_pro_pai_codigo == codigoPais && x.ciu_pro_codigo == codigoProvincia && x.ciu_codigo == codigoCiudad).FirstOrDefault();
+                    if (ciudad != null && !string.IsNullOrWhiteSpace(ciudad.ciu_pro_pai_codigo) && !string.IsNullOrWhiteSpace(ciudad.ciu_pro_codigo) && !string.IsNullOrWhiteSpace(ciudad.ciu_codigo))
+                        return Json(new { existe = true }, JsonRequestBehavior.AllowGet);
+                    else
+                        return Json(new { existe = false }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+                return Json(new { existe = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>  
+        /// Insert New Ciudad  
+        /// </summary>  
+        /// <param name="Ciudad"></param>  
+        /// <returns></returns>  
+        public string Insert(Ciudad Ciudad)
+        {
+            if (Ciudad != null)
+                using (EmpDBEntities Obj = new EmpDBEntities())
+                {
+                    Obj.Ciudades.Add(Ciudad);
+                    Obj.SaveChanges();
+                    return "Se agregó la ciudad satisfactoriamente.";
+                }
+            else
+                return "No se pudo agregar la ciudad, intente nuevamente.";
+        }
+
+        /// <summary>  
+        /// Delete Ciudad
+        /// </summary>  
+        /// <param name="Ciudad"></param>  
+        /// <returns></returns>  
+        public string Delete(Ciudad Ciudad)
+        {
+            if (Ciudad != null)
+                using (EmpDBEntities Obj = new EmpDBEntities())
+                {
+                    var Ciudad_ = Obj.Entry(Ciudad);
+                    if (Ciudad_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.Ciudades.Attach(Ciudad);
+                        Obj.Ciudades.Remove(Ciudad);
+                    }
+                    Obj.SaveChanges();
+                    return "Se eliminó la ciudad satisfactoriamente.";
+                }
+            else
+                return "No se pudo eliminar la ciudad, intente nuevamente.";
+        }
+
+        /// <summary>  
+        /// Update Ciudad  
+        /// </summary>  
+        /// <param name="Ciudad"></param>  
+        /// <returns></returns>  
+        public string Update(Ciudad Ciudad)
+        {
+            if (Ciudad != null)
+                using (EmpDBEntities Obj = new EmpDBEntities())
+                {
+                    var Ciudad_ = Obj.Entry(Ciudad);
+                    Ciudad CiudadObj = Obj.Ciudades.Where(x => x.ciu_pro_pai_codigo == Ciudad.ciu_pro_pai_codigo && x.ciu_pro_codigo == Ciudad.ciu_pro_codigo && x.ciu_codigo == Ciudad.ciu_codigo).FirstOrDefault();
+                    CiudadObj.ciu_nombre = Ciudad.ciu_nombre;
+                    Obj.SaveChanges();
+                    return "Se actualizó la ciudad satisfactoriamente.";
+                }
+            else
+                return "No se pudo actualizar la ciudad, intente nuevamente.";
         }
     }
 }
---
+++ ABMCEmpleados/Scripts/CRUDCiudad.js
@@ -0,0 +1,235 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    //debugger;
+    $http({
+        method: 'GET',
+        url: '/Ciudad/GetPaises'
+    }).then(function (response) {
+        //console.log(response);
+        $scope.paises = response.data;
+        $scope.currentPais = $scope.paises[0].pai_codigo;
+        $scope.GetAllByPais($scope.paises[0].pai_codigo);
+    }).catch(function (reason) {
+        console.log(reason);
+        alert("Ocurrió un error al intentar obtener los países.");
+    })
+
+    $scope.GetAllByPais = function (pai_codigo) {
+        //debugger;
+        $http({
+            method: 'GET',
+            url: '/Ciudad/GetProvincias',
+            params: ({ codigoPais: pai_codigo })
+        }).then(function (response) {
+            //console.log(response);
+            $scope.provincias = response.data;
+            $scope.currentProvincia = $scope.provincias[0].pro_codigo;
+            $scope.GetAllByPaisAndProvincia($scope.provincias[0].pro_pai_codigo, $scope.provincias[0].pro_codigo);
+        }).catch(function (reason) {
+            console.log(reason);
+            alert("Ocurrió un error al intentar obtener las provincias.");
+        })
+    }
+
+    //debugger;
+    $scope.Add = function () {
+        var isValid = Validate();
+
+        if (isValid == true) {
+            var Action = $("#btnSave").val();
+            if (Action == "Agregar") {
+                var ciu_pro_pai_codigo = $('#inputPais').val().trim();
+                var ciu_pro_codigo = $('#inputProvincia').val().trim();
+                var ciu_codigo = $('#inputCodigo').val().trim();
+                $.ajax({
+                    type: "GET",
+                    url: "/Ciudad/GetByKey",
+                    contentType: "application/json;charset=UTF-8",
+                    dataType: "json",
+                    data: ({ codigoPais: ciu_pro_pai_codigo, codigoProvincia: ciu_pro_codigo, codigoCiudad: ciu_codigo }),
+                    success: function (data) {
+                        if (data.existe == false) {
+                            $scope.Ciudad = {};
+                            //console.log($('#inputPais').val());
+                            $scope.Ciudad.ciu_pro_pai_codigo = $scope.currentPais;
+                            $scope.Ciudad.ciu_pro_codigo = $scope.currentProvincia;
+                            $scope.Ciudad.ciu_codigo = $scope.CodigoCiudad;
+                            $scope.Ciudad.ciu_nombre = $scope.NombreCiudad;
+                            $http({
+                                method: "POST",
+                                url: "/Ciudad/Insert",
+                                datatype: "json",
+                                data: JSON.stringify($scope.Ciudad)
+                            }).then(function (response) {
+                                alert(response.data);
+                                $scope.GetAllByPaisAndProvincia($scope.currentPais, $scope.currentProvincia);
+                                $scope.Ciudad.ciu_pro_pai_codigo = "";
+                                $scope.Ciudad.ciu_pro_codigo = "";
+                                $scope.Ciudad.ciu_codigo = "";
+                                $scope.Ciudad.ciu_nombre = "";
+                                $('#myModal').modal('hide');
+                            })
+                        } else {
+                            alert("El código de la ciudad ingresado ya existe.");
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        console.log(xhr);
+                        console.log(status);
+                        console.log(error);
+                        a = xhr.responseText;
+                        alert("Ocurrió un error al intentar verificar el código de ciudad.");
+                    }
+                })
+            } else {
+                var ciu_pro_pai_codigo = $('#inputPais').val().trim();
+                var ciu_pro_codigo = $('#inputProvincia').val().trim();
+                var ciu_codigo = $('#inputCodigo').val().trim();
+                $.ajax({
+                    type: "GET",
+                    url: "/Ciudad/GetByKey",
+                    contentType: "application/json;charset=UTF-8",
+                    dataType: "json",
+                    data: ({ codigoPais: ciu_pro_pai_codigo, codigoProvincia: ciu_pro_codigo, codigoCiudad: ciu_codigo }),
+                    success: function (data) {
+                        if (data.existe == true) {
+                            $scope.Ciudad = {};
+                            //console.log($('#inputPais').val());
+                            $scope.Ciudad.ciu_pro_pai_codigo = $scope.currentPais;
+                            $scope.Ciudad.ciu_pro_codigo = $scope.currentProvincia;
+                            $scope.Ciudad.ciu_codigo = $scope.CodigoCiudad;
+                            $scope.Ciudad.ciu_nombre = $scope.NombreCiudad;
+                            $http({
+                                method: "POST",
+                                url: "/Ciudad/Update",
+                                datatype: "json",
+                                data: JSON.stringify($scope.Ciudad)
+                            }).then(function (response) {
+                                alert(response.data);
+                                $scope.GetAllByPaisAndProvincia($scope.currentPais, $scope.currentProvincia);
+                                $scope.Ciudad.ciu_pro_pai_codigo = "";
+                                $scope.Ciudad.ciu_pro_codigo = "";
+                                $scope.Ciudad.ciu_codigo = "";
+                                $scope.Ciudad.ciu_nombre = "";
+                                $("#btnSave").attr("value", "Agregar");
+                                $('#myModal').modal('hide');
+                            })
+                        } else {
+                            alert("El código de ciudad ingresado no existe.");
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        console.log(xhr);
+                        console.log(status);
+                        console.log(error);
+                        a = xhr.responseText;
+                        alert("Ocurrió un error al intentar verificar el código de ciudad.");
+                    }
+                })
+            }
+        }
+    };
+
+    $scope.GetAll = function () {
+        //debugger;
+        $http({
+            method: "GET",
+            url: "/Ciudad/GetAll"
+        }).then(function (response) {
+            //console.log(response);
+            $scope.ciudades = response.data;
+        }).catch(function (reason) {
+            console.log(reason);
+            alert("Ocurrió un error al intentar obtener las ciudades.");
+        })
+    };
+
+    $scope.GetAllByPaisAndProvincia = function (pai_codigo, pro_codigo) {
+        //debugger;
+        $http({
+            method: "GET",
+            url: "/Ciudad/GetAllByPaisAndProvincia",
+            params: ({ codigoPais: pai_codigo, codigoProvincia: pro_codigo })
+        }).then(function (response) {
+            //console.log(response);
+            $scope.ciudades = response.data;
+        }).catch(function (reason) {
+            console.log(reason);
+            alert("Ocurrió un error al intentar obtener las ciudades por país y provincia.");
+        })
+    };
+
+    $scope.Delete = function (Ciudad) {
+        var ans = confirm('¿Está seguro de qué quiere eliminar la ciudad?');
+        if (ans) {
+            $http({
+                method: "POST",
+                url: "/Ciudad/Delete",
+                datatype: "json",
+                data: JSON.stringify(Ciudad)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllByPaisAndProvincia($scope.currentPais, $scope.currentProvincia);
+            })
+        }
+    };
+
+    $scope.Get = function (Ciudad) {
+        $scope.CodigoPais = Ciudad.ciu_pro_pai_codigo;
+        $('#inputPais').prop('readonly', true);
+        $scope.CodigoProvincia = Ciudad.ciu_pro_codigo;
+        $('#inputProvincia').prop('readonly', true);
+        $scope.CodigoCiudad = Ciudad.ciu_codigo;
+        $('#inputCodigo').prop('readonly', true);
+        $scope.NombreCiudad = Ciudad.ciu_nombre;
+        $("#btnSave").attr("value", "Actualizar");
+        $('#myModal').modal('show');
+    }
+
+    //limpia los textbox.
+    $scope.ClearTextBox = function () {
+        //debugger;
+        var codigoPais = $('#tdPais').text();
+        $('#inputPais').val(codigoPais.trim());
+        $('#inputPais').prop('readonly', true);
+        $scope.CodigoPais = codigoPais.trim();
+        var codigoProvincia = $('#tdProvincia').text();
+        $('#inputProvincia').val(codigoProvincia.trim());
+        $('#inputProvincia').prop('readonly', true);
+        $scope.CodigoProvincia = codigoProvincia.trim();
+        $('#inputCodigo').val("");
+        $('#inputCodigo').prop('readonly', false);
+        $scope.CodigoCiudad = "";
+        $('#inputNombre').val("");
+        $scope.NombreCiudad = "";
+        $("#btnSave").attr("value", "Agregar");
+    }
+})
+
+//función para validar que los campos esten completos
+function Validate() {
+    var isValid = true;
+    if ($('#inputPais').val().trim() == "") {
+        alert("Debe completar el código del país al que pertenece la ciudad.");
+        isValid = false;
+    }
+    if ($('#inputProvincia').val().trim() == "") {
+        alert("Debe completar el código de la provincia a la que pertenece la ciudad.");
+        isValid = false;
+    }
+    if ($('#inputCodigo').val().trim() == "") {
+        alert("Debe completar el código de la ciudad.");
+        isValid = false;
+    }
+    if ($('#inputNombre').val().trim() == "") {
+        alert("Debe completar el nombre de la ciudad.");
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+//esconde el modal cuando se hace click en el botón cerrar o en la cruz.
+function dismissModal() {
+    $('#myModal').modal('hide');
+}
